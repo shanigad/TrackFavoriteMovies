4014e029ddf2ae6de5e133fc5cb4d299022596cf
shanig4040 <shanigad@gmail.com>
2019-08-01 21:03:39 +0300

    fix: react error - each child in a list should have a unique key prop

--- src/components/pages/Movies.js
+++ src/components/pages/Movies.js
@@ -8,12 +8,10 @@ import '../../style/moviesStyle.css';
 function Movies() {
     // set the var and rerender
     const [movies, setMovies] = useState([]);
-    // const [flag, setFlag] = useState(false);
 
     // run this only at the first time this app is started
     useEffect(function (){
         // Get the data
-        // run once
         getAllMoviesApiRequest().then(data => setMovies(data.results));
         // unmount
         return () => {console.log("I'm dead")}
@@ -22,10 +20,11 @@ function Movies() {
     return (
         <div>
             <PageHeader title={"Movies"}/>
-            {/*<button onClick={() => setFlag(!flag)}>toggle</button>*/}
-            <div id="moviesMnDiv">
+            <div className="moviesMnDiv">
                  {movies.map(movie => (
-                     <Preview movie={movie}/>
+                     <span key={movie.id}>
+                        <Preview movie={movie}/>
+                     </span>
                  ))}
             </div>
         </div>
